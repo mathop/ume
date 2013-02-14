3ad8c3379dee62c84fc3d9a46b1ac685b4f94add
lerox <lerox@hotmail.com>
2013-02-14 00:01:37 -0200

    debug zero em producao, apenas

--- app/Config/core.php
+++ app/Config/core.php
@@ -32,7 +32,10 @@
  * In production mode, flash messages redirect after a time interval.
  * In development mode, you need to click the flash message to continue.
  */
-	Configure::write('debug', 2);
+	// Antigo
+	// Configure::write('debug', 2);
+	
+	Configure::write('debug', getenv('HTTP_HOST') == 'ume.pagodabox.com' ? 0 : 2);
 
 /**
  * Configure the Error handler used to handle errors for your application.  By default
